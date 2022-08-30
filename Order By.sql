ca30e4873c77026970c183940483ebb1a41ed48a
aldyhaha <aldisafrudin64@gmail.com>
2022-08-30 11:27:01 +0700

    Telah selesa mengerjakan Mysql

---
+++ Order By.sql
@@ -0,0 +1,6 @@
+use belajar_database;
+SELECT * FROM produk
+ORDER BY nama_produk ASC, harga_produk DESC;
+
+--ASC adalah memulai dari yang terkecil sampai terbesar contoh dari 1 - 100 atau A - Z
+--DESC adalah memulai dari yang besar ke terkecil 100 - 1 atau Z - A
---
+++ Where.sql
@@ -0,0 +1,6 @@
+use belajar_database;
+SELECT * FROM produk
+WHERE harga_produk='3500';
+
+--Mencari Keterangan di Tabel atau hanya memilih satu data 
+--misal harga produk 2500 otomatis yang keluar angka tersebut
